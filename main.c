2b8bb504e2cca9d41ad9da2b860408df08c9a2ee
ViPuL <vsvipul555@gmail.com>
2017-11-01 15:29:32 +0530

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ typedef struct
 	int wickets;
 } team;
 
-void init(team a, team b)
+void init(team* a, team* b)
 {
 	int i;
 	char num[11][3];
@@ -20,38 +20,38 @@ void init(team a, team b)
 		sprintf(num[i],"%d",i+1);
 	for (i=0;i<11;i++)
 	{
-		strcpy(a.player[i],"Player ");
-		strcpy(b.player[i],"Player ");
-		strcat(a.player[i],num[i]);
-		strcat(b.player[i],num[i]);
+		strcpy(a->player[i],"Player ");
+		strcpy(b->player[i],"Player ");
+		strcat(a->player[i],num[i]);
+		strcat(b->player[i],num[i]);
 	}
-	a.runs=0;
-	b.runs=0;
-	a.wickets=0;
-	b.wickets=0;
+	a->runs=0;
+	b->runs=0;
+	a->wickets=0;
+	b->wickets=0;
 }
 
-void inputnames(team a, team b)
+void inputnames(team* a, team* b)
 {
 	char choice;
 	int i;
 	printf("Enter name of Team A: ");
-	scanf(" %[^\n]s",a.name);
+	scanf(" %[^\n]s",a->name);
 	printf("Enter name of Team B: ");
-	scanf(" %[^\n]s",b.name);
+	scanf(" %[^\n]s",b->name);
 	printf("Do you wish to enter player names now? Enter y for yes and n for no. You can enter names later.\n");
 	scanf(" %c",&choice);
 	if (choice=='y')
 	{
 		for (i=0;i<11;i++)
 		{
-			printf("Enter name of player %d for team %s\n",i+1,a.name);
-			scanf(" %[^\n]s ",a.player[i]);
+			printf("Enter name of player %d for team %s\n",i+1,a->name);
+			scanf(" %[^\n]s ",a->player[i]);
 		}
 		for (i=0;i<11;i++)
 		{
-			printf("Enter name of player %d for team %s\n",i+1,b.name);
-			scanf(" %[^\n]s ",b.player[i]);
+			printf("Enter name of player %d for team %s\n",i+1,b->name);
+			scanf(" %[^\n]s ",b->player[i]);
 		}
 	}
 }
@@ -59,15 +59,15 @@ void inputnames(team a, team b)
 void printscoreboard(team a, team b)
 {
 	printf("+----------------------+----------+---------------------+\n");
-	printf("| %s \t\tvs\t\t %s |\n",a.name,b.name);
+	printf("| %s \tvs\t %s |\n",a.name,b.name);
 	printf("+----------------------+----------+---------------------+\n");
 }
 
 int main()
 {
 	team a,b;
-	init(a,b);
-	inputnames(a,b);
-//	printscoreboard(a,b);
+	init(&a,&b);
+	inputnames(&a,&b);
+	printscoreboard(a,b);
 	return 0;
 }
