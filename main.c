0c4d71ecd29bb0287c65eb76751f29fc7b4dc2eb
ViPuL <vsvipul555@gmail.com>
2017-11-08 13:47:22 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,73 +1,273 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
-typedef struct 
-{
-	char name[50];
-	char player[11][50];
-	int pruns[11];
-	int runs;
-	int wickets;
-} team;
+static int team1score=0;
+static int team2score=0;
+static int overs;
+int flag=0;
 
-void init(team* a, team* b)
+void clrscr()
 {
-	int i;
-	char num[11][3];
-	for (i=0;i<11;i++)
-		sprintf(num[i],"%d",i+1);
-	for (i=0;i<11;i++)
-	{
-		strcpy(a->player[i],"Player ");
-		strcpy(b->player[i],"Player ");
-		strcat(a->player[i],num[i]);
-		strcat(b->player[i],num[i]);
-	}
-	a->runs=0;
-	b->runs=0;
-	a->wickets=0;
-	b->wickets=0;
+	system("@cls||clear");
 }
 
-void inputnames(team* a, team* b)
+void printScore(char a[], char b[], int scoreteam1, int scoreteam2, int wickets1, int wickets2)
 {
-	char choice;
-	int i;
-	printf("Enter name of Team A: ");
-	scanf(" %[^\n]s",a->name);
-	printf("Enter name of Team B: ");
-	scanf(" %[^\n]s",b->name);
-	printf("Do you wish to enter player names now? Enter y for yes and n for no. You can enter names later.\n");
-	scanf(" %c",&choice);
-	if (choice=='y')
-	{
-		for (i=0;i<11;i++)
-		{
-			printf("Enter name of player %d for team %s\n",i+1,a->name);
-			scanf(" %[^\n]s ",a->player[i]);
-		}
-		for (i=0;i<11;i++)
-		{
-			printf("Enter name of player %d for team %s\n",i+1,b->name);
-			scanf(" %[^\n]s ",b->player[i]);
-		}
-	}
+    printf("\t\t\t##########################################################\n");
+    printf("\t\t\t##########################################################\n\n");
+    printf("\t\t\t%s %d/%d\n",a,scoreteam1,10-wickets1);
+    printf("\t\t\t%s %d/%d",b,scoreteam2,10-wickets2);
+    if (!flag)
+    	printf("\tYet to bat\n");
+    printf("\n");
+    printf("\t\t\t##########################################################\n");
+    printf("\t\t\t##########################################################\n\n");
 }
 
-void printscoreboard(team a, team b)
+void printResult(char a[], char b[])
 {
-	printf("+----------------------+----------+---------------------+\n");
-	printf("| %s \tvs\t %s |\n",a.name,b.name);
-	printf("+----------------------+----------+---------------------+\n");
+	clrscr();
+    printf("Match over\a\n");
+    if (team1score>team2score)
+        printf("\t\t\t\t\t%s wins\n\n\n\n\n",a);
+    else
+        printf("\t\t\t\t\t%s wins\n\n\n\n\n",b);;
 }
 
 int main()
 {
-	team a,b;
-	init(&a,&b);
-	inputnames(&a,&b);
-	printscoreboard(a,b);
+	char x;
+	printf("Do you want to start a new match? Enter y for yes and n for no.\n");
+	scanf(" %c",&x);
+	if (x=='y' || x=='Y')
+	{
+		char name1[50],name2[50],tmp[50],act[10];
+		printf("Enter name of first team.\n");
+		scanf(" %s", name1);
+		printf("Enter name of second team.\n");
+		scanf("%s", name2);
+		printf("Enter number of overs in each innings.\n");
+		scanf(" %d", &overs);
+		int balls=6*overs;
+		printf("Which team is batting first, %s or %s ?\n",name1,name2);
+		scanf(" %s", tmp);
+		if (strcmp(tmp,name1)==0)
+		{
+			int wickets1=10;
+			int wickets2=10;
+			char tmp2[50];
+			while (wickets1 && balls)
+			{
+				printScore(name1, name2, team1score, team2score, wickets1, wickets2);
+				printf("What happened on this delivery?\n");
+				printf("Wd for WideBall\nW for wicket\nNb for NO ball\nOr runs scored( <=0 )");
+				printf("\nWdW for WideBall+Wicket\nNbW for NO ball+Wicket\nWd(Runs) for Wideball+Runs\nNb(Runs) for NOball+Runs\n");
+				scanf(" %s", act);
+				if (strcmp(act,"Wd")==0)
+                {
+                    team1score++;
+                }
+                else if (strcmp(act,"Nb")==0)
+                {
+                    team1score++;
+                }
+                else if (strcmp(act,"W")==0)
+                {
+                    wickets1--;
+                    balls--;
+                }
+                else if (strcmp(act,"WdW")==0)
+                {
+                    wickets1--;
+                    team1score++;
+                }
+                else if (strcmp(act,"NbW")==0)
+                {
+                    wickets1--;
+                    team1score++;
+                }
+                else if (act[0]=='W' && act[1]=='d')
+                {
+                    team1score++;
+                    team1score+=((int)act[2]-'0');
+                }
+                else if (act[0]=='N' && act[1]=='b')
+                {
+                    team1score++;
+                    team1score+=((int)act[2]-'0');
+                }
+                else
+                {
+                    team1score+=((int)act[0]-'0');
+                    balls--;
+                }
+            }
+            printf("First Innings over\a\n");
+            flag=1;
+            balls=6*overs;
+
+            while (balls && wickets2)
+            {
+                printScore(name1, name2, team1score, team2score, wickets1, wickets2);
+                printf("What happened on this delivery?\n");
+                printf("Wd for WideBall\nW for wicket\nNb for NO ball\nOr runs scored( <=0 )\n");
+                printf("WdW for WideBall+Wicket\nNbW for NO ball+Wicket\nWd(Runs) for Wideball+Runs\nNb(Runs) for NOball+Runs\n");
+                scanf(" %s",act);
+                if (strcmp(act,"Wd")==0)
+                {
+                    team2score++;
+                }
+                else if (strcmp(act,"Wd")==0)
+                {
+                    team2score++;
+                }
+                else if (strcmp(act,"W")==0)
+                {
+                    wickets2--;
+                    balls--;
+                }
+                else if (strcmp(act,"WdW")==0)
+                {
+                    wickets2--;
+                    team2score++;
+                }
+                else if (strcmp(act,"NbW")==0)
+                {
+                    wickets2--;
+                    team2score++;
+                }
+                else if (act[0]=='W' && act[1]=='d')
+                {
+                    team2score++;
+                    team2score+=((int)act[2]-'0');
+                }
+                else if (act[0]=='N' && act[1]=='b')
+                {
+                    team2score++;
+                    team2score+=((int)act[2]-'0');
+                }
+                else
+                {
+                    team2score+=((int)act[0]-'0');
+                    balls--;
+                }
+                if(team2score>team1score)
+                {
+                    printResult(name1,name2);
+                    break;
+                }
+            }
+
+        }
+        else
+        {
+            int wickets2=10;
+			int wickets1=10;
+			char tmp2[50];
+			while (wickets2 && balls)
+			{
+				printScore(name2, name1, team2score, team1score, wickets2, wickets1);
+				printf("What happened on this delivery?\n");
+				printf("Wd for WideBall\nW for wicket\nNb for NO ball\nOr runs scored( <=0 )\n");
+				printf("\nWdW for WideBall+Wicket\nNbW for NO ball+Wicket\nWd(Runs) for Wideball+Runs\nNb(Runs) for NOball+Runs\n");
+				fgets(act,10,stdin);
+				if (strcmp(act,"Wd")==0)
+                {
+                    team2score++;
+                }
+                else if (strcmp(act,"Nb")==0)
+                {
+                    team2score++;
+                }
+                else if (strcmp(act,"W")==0)
+                {
+                    wickets2--;
+                    balls--;
+                }
+                else if (strcmp(act,"WdW")==0)
+                {
+                    wickets2--;
+                    team1score++;
+                }
+                else if (strcmp(act,"NbW")==0)
+                {
+                    wickets2--;
+                    team2score++;
+                }
+                else if (act[0]=='W' && act[1]=='d')
+                {
+                    team2score++;
+                    team2score+=((int)act[2]-'0');
+                }
+                else if (act[0]=='N' && act[1]=='b')
+                {
+                    team2score++;
+                    team2score+=((int)act[2]-'0');
+                }
+                else
+                {
+                    team2score+=((int)act[0]-'0');
+                    balls--;
+                }
+            }
+            printf("First Innings over\a\n");
+            flag=1;
+            balls=6*overs;
+
+            while (balls && wickets1)
+            {
+                printScore(name2, name1, team2score, team1score, wickets2, wickets1);
+                printf("What happened on this delivery?\n");
+                printf("Wd for WideBall\nW for wicket\nNb for NO ball\nOr runs scored( <=0 )\n");
+                printf("WdW for WideBall+Wicket\nNbW for NO ball+Wicket\nWd(Runs) for Wideball+Runs\nNb(Runs) for NOball+Runs\n");
+                fgets(act,10,stdin);
+                if (strcmp(act,"Wd")==0)
+                {
+                    team1score++;
+                }
+                else if (strcmp(act,"Wd")==0)
+                {
+                    team1score++;
+                }
+                else if (strcmp(act,"W")==0)
+                {
+                    wickets1--;
+                    balls--;
+                }
+                else if (strcmp(act,"WdW")==0)
+                {
+                    wickets1--;
+                    team1score++;
+                }
+                else if (strcmp(act,"NbW")==0)
+                {
+                    wickets1--;
+                    team1score++;
+                }
+                else if (act[0]=='W' && act[1]=='d')
+                {
+                    team1score++;
+                    team1score+=((int)act[2]-'0');
+                }
+                else if (act[0]=='N' && act[1]=='b')
+                {
+                    team1score++;
+                    team1score+=((int)act[2]-'0');
+                }
+                else
+                {
+                    team1score+=((int)act[0]-'0');
+                    balls--;
+                }
+                if(team2score>team1score)
+                {
+                    printResult(name1,name2);
+                    break;
+                }
+            }
+        }
+        printResult(name1,name2);
+    }
 	return 0;
 }
